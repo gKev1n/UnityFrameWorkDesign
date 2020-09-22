b1aec4ff8f8aa7774bda86549d33febbb9bd52a2
gKev1n <39189948+gKev1n@users.noreply.github.com>
2020-09-22 08:32:58 +0800

    2020922

---
+++ Assets/_FrameworkDesign2021/5.TypeEventSystem/MasterBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using _FrameworkDesign2021._4.PackageKit;
+using UnityEngine;
+
+public abstract class MasterBehaviour : MonoBehaviour
+{
+    private List<System.Action> mUnRegisterEventActions = new List<System.Action>();
+    
+    public void Register<T> (Action<T> onReceive)
+    {
+        TypeEventSystem.Register<T>(onReceive);
+        mUnRegisterEventActions.Add(() =>
+        {
+            TypeEventSystem.UnRegister<T>(onReceive);
+        });
+    }
+
+    public void Send<T>(T eventKey)
+    {
+        TypeEventSystem.Send<T>(eventKey);
+    }
+
+    public void UnRegister<T>(Action<T> onReceive)
+    {
+        TypeEventSystem.UnRegister<T>(onReceive);
+    }
+
+    public void UnregisterAll()
+    {
+        
+    }
+}
---
+++ Assets/_FrameworkDesign2021/5.TypeEventSystem/MasterBehaviourExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using _FrameworkDesign2021._4.PackageKit;
+using UnityEngine;
+
+namespace _FrameworkDesign2021
+{
+    public class MasterBehaviourExample : MasterBehaviour
+    {
+        public class EventA
+        {
+            
+        }
+
+        public class EventB
+        {
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.Register<EventA>(OnEventReceiveA);
+            this.Register<EventB>(OnEventReceiveB);
+        }
+
+        void OnEventReceiveA(EventA eventA)
+        { 
+            Debug.Log("On Event A Receive");
+        }
+
+        void OnEventReceiveB(EventB eventB)
+        {
+            Debug.Log("On Event B Receive");
+        }
+
+        private void OnDestroy()
+        {
+            // this.UnRegister<EventA>(OnEventReceiveA);
+            // this.UnRegister<EventB>(OnEventReceiveB);
+            this.UnregisterAll();
+        }
+    }
+}
