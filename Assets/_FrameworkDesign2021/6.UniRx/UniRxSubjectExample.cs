2096254fb631e27cf6c39d80676413174e64f638
gKev1n <2473998520@qq.com>
2020-09-24 19:36:32 +0800

    20200924

---
+++ Assets/_FrameworkDesign2021/6.UniRx/UniRxSubjectExample.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace _FrameworkDesign2021._6.UniRx
+{
+    public class UniRxSubjectExample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            var subject = new Subject<int>();
+
+            subject.Subscribe(number =>
+            {
+                Debug.Log(number);
+            });
+        
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnNext(3);
+        }
+    }
+}
